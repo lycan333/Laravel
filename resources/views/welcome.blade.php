1789e227be49beb42b3f6cd0dc6b53bd923e34b7
lycan333 <feruz333@gmail.com>
2018-02-01 12:19:54 +0500

    header changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Home Page
+                    Home Page 2
                 </div>
 
                 <div class="links">
